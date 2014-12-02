1524b0c56ffe32e2fe23f5215662f78cb415b941
Nathaniel Sheppard <Nsheppard01@gmail.com>
2014-12-02 11:48:41 -0800

    Updated comment blocks.

--- src/Flight.java
+++ src/Flight.java
@@ -3,5 +3,6 @@ public class Flight {
 /*
  * Flights have have a name which consists of an operator and a number.
  * Flights contain four variables, an origin, a destination, a departure time, and an arrival time.
+ * They might also have a ticket price.
  */
 }
--- src/Itinerary.java
+++ src/Itinerary.java
@@ -1,6 +1,8 @@
 
 public class Itinerary {
 /*
- * An Itinerary consists of a list of flights and airports? I'm not really sure.
+ * An Itinerary is a list of flights.
+ * It has two airports as an origin and destination, and a list of flights which will connect the two.
+ * It will record the total time and total money spent for the entire trip.
  */
 }
