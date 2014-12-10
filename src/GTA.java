3024d92f0617442200a1d4319b352913c798bbd3
Weshney <Connystance2@yahoo.com>
2014-12-10 10:15:49 -0800

    Update and rename AirportDriver.java to GTA.java

--- src/GTA.java
+++ src/GTA.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class AirportDriver{
+public class GTA{
   public static void main(String[] args) throws IOException {
     //for airports
     String fileName1 = "airportTester.txt";
@@ -11,15 +11,14 @@ public class AirportDriver{
       Scanner scan1 = new Scanner(new FileReader(fileName1));
       Scanner scan2 = new Scanner(new FileReader(fileName2));
       Airport.makeAirports(scan1);
-      (Airport.findAnAirportNamed("SEA")).printAirport();
       Flight.readLines(scan2);
       scan1.close();
       scan2.close();
       Airport.printAllAirports(); 
-      //Flight.printAllFlights(Flight.getAvalFlights());
+      Airport.printPossibleDestinations(Airport.findAnAirportNamed("SEA"));
       }
     catch (Exception e){        
-         //System.out.println("This file name can't be found. Enter a valid file name.");      
+         System.out.println("This file name can't be found or it was in the wrong format. Enter a valid file name, or enter a valid format.");      
        }
     
 
